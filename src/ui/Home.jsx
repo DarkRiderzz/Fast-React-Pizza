1d96c5a6dc74af5d72f999e9126db8b31931c148
Aakash Verma <akashverma611081@gmail.com>
2024-08-26 12:07:10 +0530

    reversing my changes

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="my-10 text-center px-4 sm:my-16">
       <h1 className="mb-8 text-xl  font-semibold text-center md:text-3xl">
-        The best delicious pizza.
+        The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
